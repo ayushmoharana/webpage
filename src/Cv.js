4472c22a128a544354eb500ace3430a83cfb6315
ayushmoharana <ayushm@ncac.torun.pl>
2023-10-25 20:07:22 +0200

    resolved CV

--- src/Cv.js
+++ src/Cv.js
@@ -1,4 +1,6 @@
 import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
 
 export default function Cv() {
   const docs = [
